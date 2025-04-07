542bf851ab05ba267f8502160548783c1d81f0df
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-07 16:43:39 -0700

    feat: implement login component and update routing to include login page

---
+++ src/login/login.tsx
@@ -0,0 +1,6 @@
+
+export const Login: React.FC = () => {
+    return (
+        <div><h1>/login</h1></div>
+    );
+};
--- src/router/App.tsx
+++ src/router/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Dashboard } from "@/components/dashboard/dashboard";
+import { Login } from "@/login/login";
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Dashboard />} />
       </Routes>
     </>
   );
