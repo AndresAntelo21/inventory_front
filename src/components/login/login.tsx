1a6941fa5d43afb26a1aca52138459c5bcc7ce1b
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-09 23:49:55 -0700

    feat: enhance login component layout and update background color in CSS

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,6 +1,8 @@
 
 export const Login: React.FC = () => {
     return (
-        <div><h1>/login</h1></div>
+        <div className="w-screen h-screen flex items-center justify-center">
+            <h1>/login</h1>
+        </div>
     );
 };
