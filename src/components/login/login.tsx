546f304ce4e04011e30723ea25ed134a7b20c186
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-10 16:10:19 -0700

    feat: update login component to remove placeholder text and enhance layout

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -2,7 +2,7 @@
 export const Login: React.FC = () => {
     return (
         <div className="w-screen h-screen flex items-center justify-center">
-            <h1>/login</h1>
+            <div className="w-200 h-130 bg-white/10 rounded-2xl z-10"></div>
         </div>
     );
 };
