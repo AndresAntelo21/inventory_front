36ad4c0b0e75bccef01c5c9bcc00d0a97c709323
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-10 16:28:18 -0700

    feat: add backdrop blur effect to login component for improved aesthetics

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -1,7 +1,7 @@
 
 export const Login: React.FC = () => {
     return (
-        <div className="w-screen h-screen flex items-center justify-center">
+        <div className="w-screen h-screen flex items-center justify-center backdrop-blur-2xl">
             <div className="w-200 h-130 bg-white/10 rounded-2xl z-10"></div>
         </div>
     );
