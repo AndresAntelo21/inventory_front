c70c88b1cf6c56dc64e17004988f2f911be623c3
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-13 22:28:41 -0700

    fix: update hover effect styling for SidebarItem component for consistency

--- src/components/sidebar/sidebar-item.tsx
+++ src/components/sidebar/sidebar-item.tsx
@@ -10,7 +10,7 @@ export const SidebarItem: React.FC<SidebarItemProps> = ({ icon, text, href }) =>
     return (
         <a
             href={href}
-            className="pl-2 text-2xl flex gap-3 py-1 items-center hover:bg-blue-950/60 hover:rounded-lg transition-all duration-300 ease-in-out text-white/80 hover:text-white"
+            className="pl-2 text-2xl flex gap-3 py-1 items-center hover:bg-blue-950/60 hover:rounded-r-lg transition-all duration-300 ease-in-out text-white/80 hover:text-white"
         >
             <img src={icon} alt={`Icono de ${text}`} className="size-7" />
             <span className="text-base whitespace-nowrap">{text}</span>
