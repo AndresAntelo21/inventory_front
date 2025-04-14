8c98a637645c60d589afa0fa1bc9ebf022f3facd
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-13 19:03:46 -0700

    fix: enhance Mampara component styling for better readability; update MapaEmpresa layout

--- src/components/mapa-empresa/mampara.tsx
+++ src/components/mapa-empresa/mampara.tsx
@@ -9,8 +9,8 @@ export const Mampara: React.FC<MamparaProps> = ({ title, ip, mac }) => {
     return (
         <div className="size-40 bg-blue-500 flex flex-col items-center justify-center rounded-2xl font-poppins font-bold text-white">
             <h1>{title}</h1>
-            <span className="text-sm">{ip}</span>
-            <span className="text-sm">{mac}</span>
+            <span className="text-sm font-semibold tracking-widest">{ip}</span>
+            <span className="text-sm font-semibold ">{mac}</span>
         </div>
     );
 };
--- src/components/mapa-empresa/mapa-empresa.tsx
+++ src/components/mapa-empresa/mapa-empresa.tsx
@@ -1,9 +1,11 @@
 import { Mampara } from "./mampara";
+
 // import Draggable from 'react-draggable';
 export const MapaEmpresa: React.FC = () => {
     return (
-        <div>
+        <div className="w-screen h-screen flex gap-2">
             <Mampara title="AGENTE 001" ip="10.24.110.101" mac="48-0F-CF-5D-34-5E" />
+            <Mampara title="AGENTE 002" ip="10.24.110.101" mac="48-0F-CF-5D-34-5E" />
         </div>
     );
 };
