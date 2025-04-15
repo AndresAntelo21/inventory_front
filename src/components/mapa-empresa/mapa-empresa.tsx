bb666f34ab0fe15862e08fcdaf1d9de0c79f1d64
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-14 17:48:37 -0700

    fix: adjust MapaEmpresa component dimensions for better layout consistency

--- src/components/mapa-empresa/mapa-empresa.tsx
+++ src/components/mapa-empresa/mapa-empresa.tsx
@@ -3,7 +3,7 @@ import { Mampara } from "./mampara";
 // import Draggable from 'react-draggable';
 export const MapaEmpresa: React.FC = () => {
     return (
-        <div className="w-screen h-screen flex gap-2">
+        <div className="w-full h-full flex gap-2">
             <Mampara title="AGENTE 001" ip="10.24.110.101" mac="48-0F-CF-5D-34-5E" />
             <Mampara title="AGENTE 002" ip="10.24.110.101" mac="48-0F-CF-5D-34-5E" />
         </div>
