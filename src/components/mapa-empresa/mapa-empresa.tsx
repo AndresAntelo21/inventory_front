e83d6d3123c576683c653f0d86aff915089917ad
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-15 19:17:55 -0700

    fix: add react-zoom-pan-pinch for enhanced map interaction and update styles

--- src/components/mapa-empresa/mapa-empresa.tsx
+++ src/components/mapa-empresa/mapa-empresa.tsx
@@ -1,16 +1,41 @@
 import { Mampara } from "./mampara";
-
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 // import Draggable from 'react-draggable';
 export const MapaEmpresa: React.FC = () => {
     return (
-        <div className="flex gap-2">
-            <Mampara title="AGENTE 001" ip="10.24.110.101" mac="48-0F-CF-5D-34-5E" />
-            <Mampara title="AGENTE 002" ip="10.24.110.102" mac="48-0F-CF-5D-34-5E" />
-            <Mampara title="AGENTE 003" ip="10.24.110.103" mac="48-0F-CF-5D-34-5E" />
-            <Mampara title="AGENTE 004" ip="10.24.110.104" mac="48-0F-CF-5D-34-5E" />
-            <Mampara title="AGENTE 005" ip="10.24.110.105" mac="48-0F-CF-5D-34-5E" />
-            <Mampara title="AGENTE 006" ip="10.24.110.106" mac="48-0F-CF-5D-34-5E" />
-            <Mampara title="AGENTE 007" ip="10.24.110.107" mac="48-0F-CF-5D-34-5E" />
+        <div className="flex gap-2 w-full h-full">
+            <TransformWrapper
+                initialScale={1}
+                minScale={0.5}
+                maxScale={3}
+                centerOnInit
+                centerZoomedOut
+                limitToBounds={true}
+            >
+                <TransformComponent >
+                    <div className="relative w-[3500px] h-[3000px] bg-amber-200">
+                        <Mampara
+                            title="AGENTE 001"
+                            ip="10.24.110.101"
+                            mac="48-0F-CF-5D-34-5E"
+
+                        />
+                        <Mampara
+                            title="AGENTE 002"
+                            ip="10.24.110.102"
+                            mac="48-0F-CF-5D-34-5F"
+
+                        />
+                        <Mampara
+                            title="AGENTE 003"
+                            ip="10.24.110.103"
+                            mac="48-0F-CF-5D-34-60"
+
+                        />
+                    </div>
+                </TransformComponent>
+            </TransformWrapper>
+
         </div>
     );
 };
