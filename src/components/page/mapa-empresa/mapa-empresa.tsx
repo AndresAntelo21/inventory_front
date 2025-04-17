c2c2f67b638a2a455d5175d4a6a318dba820f29b
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-16 23:32:00 -0700

    feat: add missing import for Buscador component in MapaEmpresa

--- src/components/page/mapa-empresa/mapa-empresa.tsx
+++ src/components/page/mapa-empresa/mapa-empresa.tsx
@@ -3,6 +3,7 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { useState } from "react";
 import { AddMamparaButton } from "@/components/page/mapa-empresa/add-mampara-button";
 import { Buscador } from "@/components/buscador/buscador";
+
 export const MapaEmpresa: React.FC = () => {
     const [isDragging, setIsDragging] = useState(false);
     const [isEditing, setIsEditing] = useState(false);
