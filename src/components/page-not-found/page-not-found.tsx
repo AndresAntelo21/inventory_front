bc6b99c2585458e8c00559cf7fcf98a3d5f32a16
Andres Antelo  Figueroa <119352695+AndresAntelo21@users.noreply.github.com>
2025-04-18 23:43:34 -0700

    feat: update 404 page content for improved user experience

--- src/components/page-not-found/page-not-found.tsx
+++ src/components/page-not-found/page-not-found.tsx
@@ -3,8 +3,8 @@ export const Error404: React.FC = () => {
     return (
         <div className="w-screen h-screen font-poppins flex flex-col gap-10 items-center justify-center bg-black">
             <div className="flex flex-col items-center gap-4 text-3xl text-white">
-                <h1>Pareces perdido</h1>
                 <img src="/page-not-found/404.png" alt="Hollow Knight lost" className="w-96" />
+                <div className="flex gap-2"><span>404</span> <div className="w-[3px] h-full bg-white"></div> <span>No se encuentra la página</span></div>
             </div>
             <a href="/" className="bg-blue-500 rounded-2xl px-15 py-2 hover:bg-blue-600 transition-all ease-in-out">Volver</a>
         </div>
